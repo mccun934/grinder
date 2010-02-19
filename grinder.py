6de75fbe95dbfc90a5bc06a3e8cfa007de1f9cc1
John Matthews <jwmatthews@gmail.com>
2010-02-19 09:23:00 -0800

    adding jwmatthews@gmail.com sync code to grinder

--- grinder.py
+++ grinder.py
@@ -15,9 +15,13 @@
 # granted to use or replicate Red Hat trademarks that are incorporated
 # in this software or its documentation.
 #
-import xmlrpclib
-from optparse import Option, OptionParser
+import gzip
 import pdb
+import StringIO
+import xmlrpclib
+import xml.dom.minidom
+
+from optparse import Option, OptionParser
 
 def processCommandline():
     "process the commandline, setting the OPTIONS object"
@@ -34,28 +38,93 @@ def processCommandline():
     OPTIONS, files = optionParser.parse_args()
 
 
+#Issues
+#1) Need add custom header
+#2) Data comes back gzipped, need to read it (seems like latest python in svn has support, yet python 2.6 doesn't)
+#3) xml message doesn't conform to xmlrpc standard
+#
+class GzipDecodedString(gzip.GzipFile if gzip else object):
+    """a file-like object to decode a response encoded with the gzip
+    method, as described in RFC 1952.
+    """
+    def __init__(self, data):
+        if not gzip:
+            raise NotImplementedError
+        self.stringio = StringIO.StringIO(data)
+        gzip.GzipFile.__init__(self, mode="rb", fileobj=self.stringio)
 
-class RhnTransport(xmlrpclib.Transport):
-    __cert_file = '/usr/share/rhn/RHNS-CA-CERT'
-    def __init__(self, auth_dict):
+    def close(self):
+        gzip.GzipFile.close(self)
+        self.stringio.close()
+
+
+##
+## Look at "extra_headers in Transport"  
+##
+class RHNTransport(xmlrpclib.Transport):
+    def __init__(self):
         xmlrpclib.Transport.__init__(self)
-        self.auth_dict = auth_dict
+        self.props = {}
+        self.addProperty("X-RHN-Satellite-XML-Dump-Version", "3.3")
 
-#    def make_connection(self,host):
-#        # cert_string = open('/usr/share/rhn/RHNS-CA-CERT', 'r').read()
-#        
-#        host_with_cert = (host, { 'cert_file' :  self.__cert_file })
-#        return xmlrpclib.SafeTransport.make_connection(self, host_with_cert)     
-       
+    def addProperty(self, key, value):
+        self.props[key] = value
+
+    def getProperty(self, key):
+        return self.props[key]
+
     def send_host(self, connection, host):
-        # print "Adding extra header"
-        connection.putheader("X-RHN-Satellite-XML-Dump-Version", "3.3")
-        # print "Adding passed in header vals"
-        for key, value in self.auth_dict.iteritems():
-            # print key, value
-            connection.putheader(key, str(value))
+        # print "self.props = ", self.props
+        for key in self.props:
+            # print "setting header for %s = %s" % (key, self.props[key])
+            connection.putheader(key, self.props[key])
 
-        xmlrpclib.Transport.send_host(self, connection, host)
+    ##
+    # Parse response (alternate interface).  This is similar to the
+    # parse_response method, but also provides direct access to the
+    # underlying socket object (where available).
+    #
+    # @param file Stream.
+    # @param sock Socket handle (or None, if the socket object
+    #    could not be accessed).
+    # @return Response tuple and target method.
+
+    def _parse_response(self, file, sock):
+        # read response from input file/socket, and parse it
+        response = ""
+        while 1:
+            if sock:
+                snippet = sock.recv(1024)
+            else:
+                snippet = file.read(1024)
+            if not snippet:
+                break
+            response += snippet
+
+        #
+        # TODO:
+        # Would prefer to grab header and read if data is gzip or not before doing below, but not
+        # sure how to access header here.
+        #
+        try: 
+            gzipDecodeStr = GzipDecodedString(response)
+            unzippedResponse = gzipDecodeStr.read()
+        except IOError, e:
+            #Error messages from RHN are not gzipped, bust most (maybe all) data calls will be
+            print "Caught exception when trying to ungzip response"
+            unzippedResponse = response
+
+        if self.verbose:
+            print "body:", repr(unzippedResponse)
+        
+        dom = xml.dom.minidom.parseString(unzippedResponse)
+        if file:
+            file.close()
+        if sock:
+            sock.close()
+
+        #HACK, __request expects what we return to support "len()", so throwing this in a list for now
+        return [dom]
 
 
 class Grinder():
@@ -79,11 +148,11 @@ class Grinder():
     def sync(self):
         print "Sync!!"
         SATELLITE_URL = "http://satellite.rhn.redhat.com/"
-        rhn = RhnTransport(dict())    
+        rhn = RHNTransport()    
         
-        satClient = xmlrpclib.ServerProxy(SATELLITE_URL + "/SAT", verbose=1, transport=rhn)
+        satClient = xmlrpclib.ServerProxy(SATELLITE_URL + "/SAT", verbose=0, transport=rhn)
         
-        print "set trace"
+        # print "set trace"
         # pdb.set_trace()
         
         retval = satClient.authentication.check(self.systemid)
@@ -93,15 +162,75 @@ class Grinder():
         auth_map = satClient.authentication.login(self.systemid)
         # print "KEY: %s " % key
 
-        rhn = RhnTransport(auth_map)
-        dumpClient = xmlrpclib.ServerProxy(SATELLITE_URL + "/SAT-DUMP/", verbose=9, transport=rhn)
+        rhn = RHNTransport()
+        um = xmlrpclib.Unmarshaller()
+        sparser = xmlrpclib.SlowParser(um)
+        dumpClient = xmlrpclib.ServerProxy(SATELLITE_URL + "/SAT-DUMP/", verbose=0, transport=rhn)
         print "*** calling product_names ***"
-        chan_fams = dumpClient.dump.product_names(self.systemid)
-        for fam in chan_fams:
-            print fam
-        chans = ['rhel-i386-server-vt-5']
-        chans_out = dumpClient.dump.channels(self.systemid)
-    
+        chan_fam_xml = dumpClient.dump.product_names(self.systemid)
+        # print str(chan_fam_xml)
+        packages = self.getChannelPackages(dumpClient, self.systemid, "rhel-i386-server-vt-5")
+        #print "Available packages = ", packages
+        pkgInfo = self.getShortPackageInfo(dumpClient, self.systemid, packages)
+        print "PackageInfo = ", pkgInfo
+
+
+    def getChannelFamilies(self, client, systemId):
+        return client.dump.channel_families(systemId)
+
+    def getChannelPackages(self, client, systemId, channelLabel):
+        dom = client.dump.channels(systemId, [channelLabel])
+        rhn_channel = dom.getElementsByTagName("rhn-channel")[0]
+        packages = rhn_channel.getAttribute("packages")
+        return packages.split(" ")
+
+    def getShortPackageInfo(self, client, systemId, listOfPackages):
+        dom = client.dump.packages_short(systemId, listOfPackages)
+        #Example of data
+        # <rhn-package-short name="perl-Sys-Virt" package-size="137602" md5sum="dfd888260a1618e0a2cb6b3b5b1feff9" 
+        #  package-arch="i386" last-modified="1251397645" epoch="" version="0.2.0" release="4.el5" id="rhn-package-492050"/>
+        #
+        rhn_package_shorts = dom.getElementsByTagName("rhn-package-short")
+        packages = {}
+        for pkgShort in rhn_package_shorts:
+            name, info = self.convertPkgShortToDict(pkgShort)
+            packages[name] = info
+
+        return packages
+
+    def formNEVRA(self, info):
+        nevra = info["name"]
+        epoch = info["epoch"]
+        if epoch:
+            nevra += "-" + epoch + ":"
+        nevra += info["version"] + "-" + info["release"]
+        arch = info["arch"]
+        if arch:
+            nevra += "." + arch
+        nevra += ".rpm"
+        return nevra
+
+    def formFetchName(self, info):
+        release_epoch = info["release"] + ":" + info["epoch"]
+        return info["name"] + "-" + info["version"] + "-" + release_epoch + "." + info["arch"] + ".rpm"
+
+    def convertPkgShortToDict(self, pkgShort):
+        info = {}
+        info["name"] = pkgShort.getAttribute("name")
+        info["package_size"] = pkgShort.getAttribute("package-size")
+        info["md5sum"] = pkgShort.getAttribute("md5sum")
+        info["arch"] = pkgShort.getAttribute("package-arch")
+        info["last_modified"] = pkgShort.getAttribute("last-modified")
+        info["epoch"] = pkgShort.getAttribute("epoch")
+        info["version"] = pkgShort.getAttribute("version")
+        info["release"] = pkgShort.getAttribute("release")
+        info["id"] = pkgShort.getAttribute("id")
+        info["fetch_name"] = self.formFetchName(info)
+        nevra = self.formNEVRA(info)
+        return nevra, info
+
+
+
     
 
 if __name__ != '__main__':
