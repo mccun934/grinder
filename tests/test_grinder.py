8cc7d84c18a0d1b17e61d0921c160c50fd4ab455
Mike McCune <mmccune@redhat.com>
2010-02-26 13:46:01 -0800

    adding more readme and unit test harness

---
+++ tests/test_grinder.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestGrinder(unittest.TestCase):
+
+    def test_isPkgShortNewer(self):
+        print "Foo"
+        assert(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
